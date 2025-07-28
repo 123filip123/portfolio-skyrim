8e27648f39b92b25c9e950f5c875788b36bae548
Filip Popovski <filip.popovski@proton.me>
2025-07-28 15:04:57 +0200

    update button on hover change colors

--- app/ui/button/button.tsx
+++ app/ui/button/button.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Image from "next/image";
-
 interface IButtonProps {
   text: string;
   onClick?: () => void;
@@ -11,27 +9,47 @@ export const Button = ({ text, onClick }: IButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="relative inline-flex items-center justify-center cursor-pointer"
+      className="relative inline-flex items-center justify-center cursor-pointer group"
     >
       {/* outer border */}
-      <Image
-        src="/button/outer-border.png"
-        alt="Button outer border"
-        width={250}
-        height={42}
-      />
+      <svg
+        width="251"
+        height="44"
+        viewBox="0 0 251 44"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M16.75 1H233L249.5 22L233 43H16.75L1 22L16.75 1Z"
+          stroke="#D9D9D9"
+          strokeOpacity="0.7"
+          strokeWidth="1.5"
+          className="group-hover:fill-white group-hover:stroke-black group-hover:stroke-opacity-100 transition-all duration-200"
+        />
+      </svg>
 
       {/* inner border */}
-      <Image
-        src="/button/inner-border.png"
-        alt="Button inner border"
-        width={240}
-        height={36}
+      <svg
+        width="243"
+        height="38"
+        viewBox="0 0 243 38"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-      />
+      >
+        <path
+          d="M14.481 1H226.993L241.5 19L226.993 37H14.481L1 19L14.481 1Z"
+          fill="#0D0D0D"
+          fillOpacity="0.6"
+          stroke="#D9D9D9"
+          strokeOpacity="0.7"
+          strokeWidth="1.5"
+          className="group-hover:fill-white group-hover:stroke-black group-hover:stroke-opacity-100 transition-all duration-200"
+        />
+      </svg>
 
       {/* text */}
-      <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-medium text-xl">
+      <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-medium text-xl group-hover:text-black group-hover:!text-shadow-none transition-colors duration-200">
         {text}
       </span>
     </button>
